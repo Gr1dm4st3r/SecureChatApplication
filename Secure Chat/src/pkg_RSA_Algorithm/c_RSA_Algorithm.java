155a4a2893eb9da4df2f432b83a916ea28a15be7
Michael O Brien <mikeyob92@hotmail.com>
2013-06-11 15:43:27 +0100

    Created the RSA Algorithm Class to hold the logic for the encryptMessage, decryptMessage and keyGeneration.

---
+++ Secure Chat/src/pkg_RSA_Algorithm/c_RSA_Algorithm.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg_RSA_Algorithm;
+
+import java.math.BigInteger;
+
+/**
+ *
+ * @author HOME PC
+ */
+public class c_RSA_Algorithm {
+    //Note at the moment this is a test method to see if commits are pushed to the GitHub Repo online.
+    public static BigInteger encryptMessage(){
+        return new BigInteger("1");
+    }
+}
