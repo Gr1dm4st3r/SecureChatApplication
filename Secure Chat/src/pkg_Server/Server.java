06f0ea304585a808f3cad9b5f427bc89395f5558
Michael O Brien <mikeyob92@hotmail.com>
2013-07-14 16:34:21 +0100

    - Added variable to hold clients: clientOutputStreams. - Added methods: go, run, sendToEveryone. - Added an inner class called ClientHandler which reads puts the incoming clients trying to connect into the ArrayList clientOutputStreams.

--- Secure Chat/src/pkg_Server/Server.java
+++ Secure Chat/src/pkg_Server/Server.java
@@ -1,13 +1,53 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * 
+ * 
  */
 package pkg_Server;
+
+import java.io.BufferedReader;
+import java.net.Socket;
+import java.util.ArrayList;
 
 /**
  *
  * @author Michael O Brien | T00150213
  */
+
+//To create an instance of the server write "new Server().go();"
 public class Server {
+    
+    ArrayList clientOutputStreams; //Where information about the clients are stored on the server
+    
+    public void run(){
+        String Message; //Read the message in from the reader and send it out to the clients
+    }
+    
+    public void go(){
+        clientOutputStreams = new ArrayList();
+        
+        //Give the server a socket
+        //Accept new clients output streams and add to the arraylist
+        //Create a new thread for the client
+    }
+    
+    public void sendToEveryone(String Message){
+        //Iterate through the arraylist and flush the message out
+    }
+    
+    //Class to handle the Clients
+    public class ClientHandler implements Runnable{
+        BufferedReader reader;
+        Socket sock;
+        
+        public ClientHandler(Socket clientSocket){
+            //Get the client socket and get the input stream and add as a new BufferedReader
+        }
+        
+        @Override
+        public void run(){
+            String message;
+            //While there is a message coming in send it out to everyone
+        }
+    }
     
 }
